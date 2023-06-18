fe32909109810c787f1578d8e63d4ea7f7a8a1be
João Pimentel <joao_pimentel98@hotmail.com>
2023-06-18 19:22:12 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of e3fdb70 (git status)
 document.getElementById("exit-overlay").addEventListener("click", function () {
   let containerOne = document.getElementById("gradient-section");
   containerOne.style.display = "flex";
   let overlayContainer = document.getElementById("overlay-container");
   overlayContainer.style.display = "none";
 });
+<<<<<<< HEAD
 
 document.getElementById("rule-btn-main").addEventListener("click", function () {
   let OverlayContainerMain = document.getElementById("overlay-container");
   OverlayContainerMain.style.display = "flex";
 });
+=======
+>>>>>>> parent of e3fdb70 (git status)
